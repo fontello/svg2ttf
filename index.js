2a545e4fd6a8f2dcfdf863b6b45feb7b62a15a19
Sergey Batishchev <sergej.batishchev@gmail.com>
2013-07-05 02:09:24 +0400

    New engine (Font+TTF+SVG) works now.

--- index.js
+++ index.js
@@ -9,20 +9,45 @@
 'use strict';
 
 var _ = require('lodash');
-var svg_font = require("./lib/svg_font");
-var ttfFont = require("./lib/ttf_utils");
-var TTF = require("./lib/ttf");
+var svg_font = require("./lib/svg");
+var Font = require("./lib/font");
+
+function loadSVG(svg) {
+  var font = new Font.Font();
+  var svgFont = svg_font(svg);
+  font.id = svgFont.id;
+  font.familyName = svgFont.familyName;
+  font.copyright = svgFont.copyright;
+  _.forEach(svgFont.glyphs, function (svgGlyph) {
+    var glyph = new Font.Glyph();
+    glyph.id = svgGlyph.id;
+    glyph.unicode = svgGlyph.unicode;
+    glyph.name = svgGlyph.name;
+    glyph.isMissed = svgGlyph.isMissed;
+    glyph.height = svgGlyph.height;
+    glyph.width = svgGlyph.width;
+    glyph.size = svgGlyph.size;
+    _.forEach(svgGlyph.contours, function (svgContour) {
+      var contour = new Font.Contour();
+      _.forEach(svgContour.getQuadSplinePoints(), function (svgPoint) {
+        var point = new Font.Point();
+        point.x = svgPoint.x;
+        point.y = svgPoint.y;
+        point.onCurve = svgPoint.onCurve;
+        contour.points.push(point);
+      });
+      glyph.contours.push(contour);
+    });
+    font.glyphs.push(glyph);
+  });
+  return font;
+}
 
 //------------------Main---------------------------------
 
 function svg2ttf(svg, options, callback) {
-  var font = svg_font(svg);
-  var ttf = new TTF();
-  _.forEach(font.glyphs, function (glyph) {
-    ttfFont.addGlyph(font, ttf, glyph);
-  });
-  ttfFont.finalize(font, ttf);
-  callback(null, ttf.toBuffer());
+  var font = loadSVG(svg);
+  callback(null, font.generateTTF());
 }
 
 module.exports = svg2ttf;
