80529bd6b5d4afa1f81274c4a457586cabe1b8db
Sergey Batishchev <sergej.batishchev@gmail.com>
2013-07-05 16:08:13 +0400

    font.js is renamed to sfmt.js.

--- index.js
+++ index.js
@@ -10,7 +10,7 @@
 
 var _ = require('lodash');
 var svg_font = require("./lib/svg");
-var Font = require("./lib/font");
+var Font = require("./lib/sfmt");
 
 function loadSVG(svg) {
   var font = new Font.Font();
