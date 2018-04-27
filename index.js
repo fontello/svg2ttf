8554dd900497cdd8522fb4cd07313dc5645260d0
headshot4good <mihaiflorinmoldovan@yahoo.com>
2018-04-27 10:34:36 +0300

    Update index.js
    + replaced [TAB] with [SPACE][SPACE]

--- index.js
+++ index.js
@@ -24,7 +24,7 @@ function svg2ttf(svgString, options) {
   options = options || {};
 
   font.id = svgFont.familyName || options.id || svgFont.id;
-	svgFont.id = font.id;
+  svgFont.id = font.id;
   font.familyName = options.familyname || svgFont.familyName || svgFont.id;
   font.copyright = options.copyright || svgFont.metadata;
   font.sfntNames.push({ id: 2, value: options.subfamilyname || svgFont.subfamilyName || 'Regular' }); // subfamily name
